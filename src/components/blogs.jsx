00c57faae0825db1b2a0814f71dfa0e88deae8b9
roran williams <126867403+roranwilliam@users.noreply.github.com>
2024-02-14 06:17:39 +0000

    finish refactore cardPost component

---
+++ src/components/blogs.jsx
@@ -0,0 +1,20 @@
+const cardBlogs = [
+  {
+    topic: "World",
+    title: "Featured post",
+    date: "Nov 12",
+    content:
+      "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+    image: "https://source.unsplash.com/200x250/?food",
+  },
+  {
+    topic: "World",
+    title: "Featured post",
+    date: "Nov 12",
+    content:
+      "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+    image: "https://source.unsplash.com/200x250/?lifestyle",
+  },
+];
+
+export default cardBlogs;
